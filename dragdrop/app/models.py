d231ee61fa23fe75c1bc601595c40fe28b2914a3
yuya <tabayu5@gmail.com>
2019-12-19 09:26:21 +0900

    html

--- dragdrop/app/models.py
+++ dragdrop/app/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-<html>
-<head>
-    <title>DesignTracer</title>
-    # <link rel="stylesheet" type="text/css" href="{% static 'css/style.css' %}">
-</head>
-
-
-
-</html>
+class Image(models.Model):
+    image = models.ImageField(upload_to='images')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image
--- dragdrop/app/views.py
+++ dragdrop/app/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,redirect,render
+from django.contrib.auth.models import User
+from .models import Image
+from .forms import ImageForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request, 'blogs/index.html')
+    if request.method == "POST":
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = request.user
+            image.save()
+            messages.success(request, "投稿が完了しました！")
+        return redirect('app:users_detail', pk=request.user.pk)
+    else:
+        form = ImageForm()
+    return render(request, 'index.html', {'form':form})
