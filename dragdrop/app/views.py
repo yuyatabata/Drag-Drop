a72957befe43b0e5f8512b45980d6614ad622aa2
yuya <tabayu5@gmail.com>
2019-12-25 22:11:41 +0900

    uploaded_file_url

--- dragdrop/app/views.py
+++ dragdrop/app/views.py
@@ -2,31 +2,34 @@ from django.shortcuts import get_object_or_404,redirect,render
 from .models import Image
 from django.contrib import messages
 import os
-
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request):
-    fname = '../dragdrop/images/testfiles.jpg'
+    # fname = '../dragdrop/images/testfiles.jpg'
     if request.method == "POST":
-        updata = request.FILES.get('image')
+        updata = request.FILES['image']
         print("updata:",updata)
         print("FILES:",request.FILES)
         print("POST:",request.POST)
         print("path:",request.path)
-        f = open(fname,'wb+')
-        print("f:",f)
-        # form = ImageForm(request.POST, request.FILES)
-        # if form.is_valid():
+        # f = open(fname,'wb+')
+        # print("f:",f)
         if updata != None:
-            for chunk in updata.chunks():
-                f.write(chunk)
-                print("f",f)
-            f.close()
-            img = Image()
-            img.image_f = f
-            print(img.image_f.url)
-            images = img.objects.all().order_by("-created_at")
-        return render(request, 'index.html', {'images':images})
+            fs = FileSystemStorage()
+            filename = fs.save(updata.name, updata)
+            uploaded_file_url = fs.url(filename)
+            print(uploaded_file_url)
+            # for chunk in updata.chunks():
+            #     f.write(chunk)
+            #     print("f",f)
+            # f.close()
+            # img = Image()
+            # img.image_f = f
+            # print(img.image_f.url)
+            # images = img.objects.all().order_by("-created_at")
+        return render(request, 'index.html', {'uploaded_file_url':uploaded_file_url})
     else:
         # form = ImageForm()
         # images = Image.objects.all()
--- dragdrop/dragdrop/settings.py
+++ dragdrop/dragdrop/settings.py
@@ -22,8 +22,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #画像を保存するディレクトリ
-MEDIA_ROOT = os.path.join(BASE_DIR, '/')
-MEDIA_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images/')
+MEDIA_URL = '/images/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
